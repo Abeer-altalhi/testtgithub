c8cc89c3d3c0f55379b32c504587e41cbb6d69e9
Abeer Altalhi <abeerabdrabu17@gmail.com>
2022-10-27 12:05:23 +0300

    Initial Commit

---
+++ testtgithub/testtgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtgithubApp.swift
+//  testtgithub
+//
+//  Created by Abeer on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testtgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
